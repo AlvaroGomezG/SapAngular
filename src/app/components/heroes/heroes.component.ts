9a03088fed9de7e7cdb77673a3e5bc186d5d8984
algomez <algomez@Sistran.com.co>
2019-08-09 15:32:17 -0500

    segundo commit servicios

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
+  title: string ;
+  titleSmall: string;
+  titleBtn: string;
+  data: Heroe [];
 
-  constructor() { }
+  constructor(private _heroesService: HeroesService) {
+    this.title = 'Marvel';
+    this.titleSmall = 'DC -Marvel';
+    this.titleBtn = 'Ver más...';
+   }
 
   ngOnInit() {
+   this.data = this._heroesService.getHeroes();
+  }
+
+  verHeroe( i: number) {
+  console.log(i);
   }
 
 }
